c04392ec5799af23b0b8fba45b2856f4a6bcf265
pacecama7006 <pacecama7006@gmail.com>
2021-04-22 13:49:51 -0500

    Actualizar formato números

--- public/js/detalleCompra.js
+++ public/js/detalleCompra.js
@@ -81,7 +81,10 @@ $(document).ready(function() {
         if (product_id != "" && quantity != "" && quantity > 0 && price != "") {
             subtotal[cont] = quantity * price;
             total = total + subtotal[cont];
-            var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');"><i class="fa fa-times"></i></button></td> <td><input type="hidden" name="product_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" id="price[]" name="price[]" value="' + price + '"> <input class="form-control" type="number" id="price[]" value="' + price + '" disabled> </td>  <td> <input type="hidden" name="quantity[]" value="' + quantity + '"> <input class="form-control" type="number" value="' + quantity + '" disabled> </td> <td align="right">$ ' + subtotal[cont] + ' </td></tr>';
+            var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');"><i class="fa fa-times"></i></button></td> <td><input type="hidden" name="product_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" id="price[]" name="price[]" value="' + price + '"> <input class="form-control" type="number" id="price[]" value="' + price + '" disabled> </td>  <td> <input type="hidden" name="quantity[]" value="' + quantity + '"> <input class="form-control" type="number" value="' + quantity + '" disabled> </td> <td align="right">' + new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD'
+            }).format(subtotal[cont]) + ' </td></tr>';
             cont++;
             limpiar();
             totales();
@@ -103,11 +106,20 @@ $(document).ready(function() {
     }; //Fin de la función limpiar
     //Función totales
     function totales() {
-        $("#total").html("$ " + total.toFixed(2));
+        $("#total").html(new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(total));
         total_impuesto = total * impuesto / 100;
         total_pagar = total + total_impuesto;
-        $("#total_impuesto").html("$ " + total_impuesto.toFixed(2));
-        $("#total_pagar_html").html("$ " + total_pagar.toFixed(2));
+        $("#total_impuesto").html(new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(total_impuesto));
+        $("#total_pagar_html").html(new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(total_pagar));
         $("#total_pagar").val(total_pagar.toFixed(2));
     }; //Fin de la función totales
     //Función evaluar
--- public/js/detalleVenta.js
+++ public/js/detalleVenta.js
@@ -85,7 +85,10 @@ $(document).ready(function() {
             if (parseInt(stock) >= parseInt(quantity)) {
                 subtotal[cont] = (quantity * price) - (quantity * price * discount / 100);
                 total = total + subtotal[cont];
-                var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');"><i class="fa fa-times fa-2x"></i></button></td> <td><input type="hidden" name="product_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" name="price[]" value="' + parseFloat(price).toFixed(2) + '"> <input class="form-control" type="number" value="' + parseFloat(price).toFixed(2) + '" disabled> </td> <td> <input type="hidden" name="discount[]" value="' + parseFloat(discount) + '"> <input class="form-control" type="number" value="' + parseFloat(discount) + '" disabled> </td> <td> <input type="hidden" name="quantity[]" value="' + quantity + '"> <input type="number" value="' + quantity + '" class="form-control" disabled> </td> <td align="right">$ ' + parseFloat(subtotal[cont]).toFixed(2) + '</td></tr>';
+                var fila = '<tr class="selected" id="fila' + cont + '"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar(' + cont + ');"><i class="fa fa-times fa-2x"></i></button></td> <td><input type="hidden" name="product_id[]" value="' + product_id + '">' + producto + '</td> <td> <input type="hidden" name="price[]" value="' + parseFloat(price).toFixed(2) + '"> <input class="form-control" type="number" value="' + parseFloat(price).toFixed(2) + '" disabled> </td> <td> <input type="hidden" name="discount[]" value="' + parseFloat(discount) + '"> <input class="form-control" type="number" value="' + parseFloat(discount) + '" disabled> </td> <td> <input type="hidden" name="quantity[]" value="' + quantity + '"> <input type="number" value="' + quantity + '" class="form-control" disabled> </td> <td align="right">' + new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                }).format(subtotal[cont]) + '</td></tr>';
                 cont++;
                 limpiar();
                 totales();
@@ -116,8 +119,14 @@ $(document).ready(function() {
         $("#total").html("$ " + total.toFixed(2));
         total_impuesto = total * impuesto / 100;
         total_pagar = total + total_impuesto;
-        $("#total_impuesto").html("$ " + total_impuesto.toFixed(2));
-        $("#total_pagar_html").html("$ " + total_pagar.toFixed(2));
+        $("#total_impuesto").html(new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(total_impuesto));
+        $("#total_pagar_html").html(new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(total_pagar));
         $("#total_pagar").val(total_pagar.toFixed(2));
     }; //Fin de la función totales
     //Función evaluar
